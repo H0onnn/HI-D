60933de3f0fd9b7ed2106fc199ae90a18dc35f46
H0onnn <dukei2017@naver.com>
2023-09-21 16:14:07 +0900

    Feat: global layout component 작성

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../constants/colors';
+
+interface LayoutInterface {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutInterface) => {
+  return (
+    <PageLayout>
+      <PageContentLayout>{children}</PageContentLayout>
+    </PageLayout>
+  );
+};
+
+export default Layout;
+
+const PageLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background: ${colors.gray};
+`;
+
+const PageContentLayout = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: 390px;
+  background: ${colors.white};
+  margin: 0 auto;
+  padding-bottom: 65px;
+`;
