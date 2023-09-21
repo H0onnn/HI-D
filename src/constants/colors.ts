5ecc9da33a121d0e0c7ca42eed044dbdcb35db63
H0onnn <dukei2017@naver.com>
2023-09-21 21:21:22 +0900

    Style: color constant 추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,6 +2,9 @@ export const colors = {
   primary: '#393E46',
   secondary: '#222831',
   font: '#6F6F6F',
+  inputFont: '#BABFC2',
   gray: '#A2A9AC',
+  paleGray: '#F9F8F8',
+  lineGray: '#D6D6D6',
   white: '#FFFFFF',
 } as const;
