f94ad5786b61c534faa629a786fb0eac12ea7998
H0onnn <dukei2017@naver.com>
2023-09-22 17:25:04 +0900

    Feat: status에 따른 Input 테두리 및 image 조건부 렌더링

--- src/components/public/Input.tsx
+++ src/components/public/Input.tsx
@@ -4,17 +4,28 @@ import { colors } from '../../constants/colors';
 import Button from './Button';
 
 interface InputInterface extends React.InputHTMLAttributes<HTMLInputElement> {
-  image?: string;
+  image?: string | React.ReactNode;
   button?: boolean;
   children?: React.ReactNode;
+  status?: 'default' | 'success' | 'error';
 }
 
+const colorMap = {
+  default: colors.primary,
+  success: colors.success,
+  error: colors.error,
+};
+
 const Input = forwardRef<HTMLInputElement, InputInterface>(
-  ({ image, button, children, ...props }, ref) => {
+  ({ image, button, children, status = 'default', ...props }, ref) => {
     return (
-      <InputLayout>
+      <InputLayout status={status}>
         <CustomInput ref={ref} {...props} />
-        {image && <img src={image} alt='input icon' />}
+        {typeof image === 'string' ? ( // 문자열일 때는 이미지로 처리
+          <img src={image} alt='input icon' />
+        ) : (
+          image // 컴포넌트일 때는 그대로 렌더링
+        )}
         {button && (
           <Button variant='textOnly' style={{ color: colors.font }}>
             {children}
@@ -29,7 +40,7 @@ Input.displayName = 'Input';
 
 export default Input;
 
-const InputLayout = styled.div`
+const InputLayout = styled.div<{ status: 'default' | 'success' | 'error' }>`
   position: relative;
   width: 100%;
   height: 4.8rem;
@@ -39,8 +50,10 @@ const InputLayout = styled.div`
   display: flex;
   justify-content: space-between;
 
+  box-shadow: ${({ status }) => (status !== 'default' ? `0 0 0 2px ${colorMap[status]}` : 'none')};
+
   &:focus-within {
-    box-shadow: 0 0 0 2px ${colors.primary};
+    box-shadow: 0 0 0 2px ${({ status }) => colorMap[status]};
   }
 `;
 
