7d730b072ed4f8a999108760a63f3fc5cb5bc7eb
H0onnn <dukei2017@naver.com>
2023-09-23 21:24:20 +0900

    Feat: 학교명 유효성 검사 로직 추가

--- src/utils/auth/validationRules.ts
+++ src/utils/auth/validationRules.ts
@@ -15,3 +15,19 @@ export const passwordValidation = {
     message: '비밀번호는 영어, 숫자, 특수문자를 포함한 8자에서 16자 사이여야 합니다.',
   },
 };
+
+export const schoolValidation = {
+  required: '학교명은 필수 입력 항목입니다.',
+  pattern: {
+    // 한글만 허용하면서 '학교' 단어를 포함
+    value: /^[가-힣]+학교$/,
+    message: '올바른 학교명을 입력해주세요.',
+  },
+  validate: {
+    notOnlyConsonantsVowels: (value: string) => {
+      // 'ㅁㄴㅇㅁㄴㅇ'와 같은 값 방지
+      const hasConsonantVowelOnlyPattern = /^[ㄱ-ㅎㅏ-ㅣ]+$/;
+      return !hasConsonantVowelOnlyPattern.test(value) || '올바른 학교명을 입력해주세요.';
+    },
+  },
+};
