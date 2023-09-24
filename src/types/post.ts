e51efe0bd5de6b340de843b4d130e7711945018e
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-09-24 15:23:58 +0900

    Feat: post, chat, report 타입 정의

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,48 +1,46 @@
-export type Post = {
-  writer: string;
-  major: string;
-  category: Category;
-  title: string;
-  images: string[] | [];
-};
-
-export type Comment = {
-  postId: number;
-  writer: string;
-  content: string;
-};
-
-enum Category {
-  'help' = 'help',
-  'free' = 'free',
+export enum Category {
+  'help',
+  'free',
 }
-
 export type PostList = {
-  dataList: PostDetail[];
+  dataList: Post[];
   size: number;
   next: boolean;
 };
-export type PostDetail = {
+export type Post = {
   postId: number;
   writer: string;
   major: string;
-  category: Category;
+  category: keyof typeof Category;
   title: string;
   content: string;
   viewCount: number;
   recommendCount: number;
-  reportCount: number;
   replyCount: number;
   images: string[] | [];
   createAt: string;
   updateAt: string;
 };
-export type CommentList = {
-  dataList: CommentDetail[];
+export type RequestPostCreate = {
+  major: string;
+  category: keyof typeof Category;
+  title: string;
+  content: string;
+};
+export type RequestPostUpdate = {
+  postId: number;
+  major?: string;
+  category?: keyof typeof Category;
+  title?: string;
+  content?: string;
+};
+
+export type ReplyList = {
+  dataList: Reply[];
   size: number;
   next: boolean;
 };
-export type CommentDetail = {
+export type Reply = {
   replyId: number;
   writer: string;
   postId: number;
@@ -50,4 +48,21 @@ export type CommentDetail = {
   recommendCount: number;
   createAt: string;
   updateAt: string;
+};
+export type RequestReplyCreate = {
+  postId: number;
+  content: string;
+};
+export type RequestReplyUpdate = {
+  replyId: number;
+  content?: string;
+};
+
+export type getPostListOptions = {
+  page?: number;
+  size?: number;
+  keyword?: string;
+  order?: string;
+  category?: string;
+  majorCategory?: string;
 };
