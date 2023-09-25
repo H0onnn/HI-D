d4ab45d2dd2796bdd90746267b71a498d9b00e81
H0onnn <dukei2017@naver.com>
2023-09-25 20:37:55 +0900

    Feat: 전공, 닉네임, 비밀번호 확인 유효성 검사 추가

--- src/utils/auth/validationRules.ts
+++ src/utils/auth/validationRules.ts
@@ -1,5 +1,5 @@
 export const emailValidation = {
-  required: '이메일을 입력해주세요.',
+  required: '이메일은 필수 입력 항목입니다.',
   pattern: {
     // 이메일 형식 검사 : ex) abc@abc.com
     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
@@ -8,13 +8,21 @@ export const emailValidation = {
 };
 
 export const passwordValidation = {
-  required: '비밀번호를 입력해주세요.',
+  required: '비밀번호는 필수 입력 항목입니다.',
   pattern: {
     // 비밀번호 형식 검사 : 영어, 숫자, 특수문자 포함 8자에서 16자 사이
     value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/,
     message: '비밀번호는 영어, 숫자, 특수문자를 포함한 8자에서 16자 사이여야 합니다.',
   },
 };
+
+export const passwordConfirmValidation = (password: string) => ({
+  validate: {
+    matchesPreviousPassword: (value: string) => {
+      return password === value || '비밀번호가 일치하지 않습니다.';
+    },
+  },
+});
 
 export const schoolValidation = {
   required: '학교명은 필수 입력 항목입니다.',
@@ -29,5 +37,38 @@ export const schoolValidation = {
       const hasConsonantVowelOnlyPattern = /^[ㄱ-ㅎㅏ-ㅣ]+$/;
       return !hasConsonantVowelOnlyPattern.test(value) || '올바른 학교명을 입력해주세요.';
     },
+  },
+};
+
+export const majorValidation = {
+  required: '학과명은 필수 입력 항목입니다.',
+  pattern: {
+    // 한글만 허용 (특수문자, 공백 제외)
+    value: /^[가-힣]+$/,
+    message: '올바른 학과명을 입력해주세요.',
+  },
+  validate: {
+    notOnlyConsonantsVowels: (value: string) => {
+      // 'ㅁㄴㅇㅁㄴㅇ'와 같은 값 방지
+      const hasConsonantVowelOnlyPattern = /^[ㄱ-ㅎㅏ-ㅣ]+$/;
+      return !hasConsonantVowelOnlyPattern.test(value) || '올바른 학과명을 입력해주세요.';
+    },
+  },
+};
+
+export const nicknameValidation = {
+  required: '닉네임은 필수 입력 항목입니다.',
+  minLength: {
+    value: 2,
+    message: '닉네임은 최소 2글자 이상이어야 합니다.',
+  },
+  maxLength: {
+    value: 8,
+    message: '닉네임은 최대 8글자 까지 허용됩니다.',
+  },
+  pattern: {
+    // 특수문자 제외, 1글자에서 8글자 사이
+    value: /^[A-Za-z0-9가-힣]{1,8}$/,
+    message: '사용할 수 없는 닉네임입니다.',
   },
 };
