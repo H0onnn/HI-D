a2517ec4f02c30e133090518e1ac654b350e001e
H0onnn <dukei2017@naver.com>
2023-09-25 20:37:29 +0900

    Feat: 공통으로 사용되는 style 추가

---
+++ src/styles/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  position: absolute;
+  bottom: 7rem;
+  left: 0;
+  padding: 0 2rem;
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+export const PageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+  padding-top: 7rem;
+`;
