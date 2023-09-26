a1ea6d823c6626c9ebbd8f502038215a283c52fb
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-09-27 00:03:05 +0900

    Feat: chat 타입 수정 및 추가

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,25 +1,37 @@
-export type ChatRoomList = {
-  dataList: ChatRoom[];
+export interface ChatRoomListInterface {
+  dataList: ChatRoomInterface[];
   size: number;
   next: boolean;
-};
-export type ChatRoom = {
+}
+export interface ChatRoomInterface {
   roomId: number;
   members: string[];
-  createAt: string;
-  updateAt: string;
-};
-export type RequestChatRoomCreate = {
+  // createAt: string;
+  // updateAt: string;
+}
+export interface RequestChatRoomCreateInterface {
   memberId: string;
-};
-export type RequestChatRoomUpdate = {
+}
+export interface RequestChatRoomUpdateInterface {
   roomId: number;
-};
-export type RequestChatRoomDelete = {
+}
+export interface RequestChatRoomDeleteInterface {
   roomId: number;
-};
-export type Chat = {
+}
+export interface ChatInterface {
   roomId: number;
   memberId: string;
   content: string;
-};
+  // 과거 채팅 기록도 가져올지?
+  nickname: string;
+  date: string;
+  chatId: number;
+}
+export interface PageStatusInterface {
+  page: number;
+  isNext: boolean;
+}
+export interface ChatModalStatusInterface {
+  isOpen: boolean;
+  roomId: number;
+}
