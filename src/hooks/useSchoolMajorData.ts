c1b375db04c847972b143195cdf37372f08cf08a
H0onnn <dukei2017@naver.com>
2023-10-03 18:10:38 +0900

    Feat: 검색시 유효성 검사 반영 및 loading 상태 추가

--- src/hooks/useSchoolMajorData.ts
+++ src/hooks/useSchoolMajorData.ts
@@ -1,24 +1,31 @@
 import { useState, useEffect } from 'react';
 import { getSchools, getMajors } from '../api/schoolData';
+import { INCOMPLETE_KOREAN_REGEX, ENGLISH_ONLY_REGEX } from '../utils/auth/validationRules';
 
 const useSchooMajorData = (fieldName: string, searchValue: string) => {
   const [datas, setDatas] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
+        if (INCOMPLETE_KOREAN_REGEX.test(searchValue) || ENGLISH_ONLY_REGEX.test(searchValue))
+          return;
         const res =
           fieldName === 'school' ? await getSchools(searchValue) : await getMajors(searchValue);
         setDatas(res);
       } catch (err) {
         console.log(err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, [fieldName, searchValue]);
 
-  return { datas };
+  return { datas, isLoading };
 };
 
 export default useSchooMajorData;
