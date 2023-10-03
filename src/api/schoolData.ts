f4bc2bbd08de56be87e79076857d7d645f5da543
H0onnn <dukei2017@naver.com>
2023-10-03 17:49:42 +0900

    Feat: 학교, 학과 데이터 get 요청 로직 작성

---
+++ src/api/schoolData.ts
@@ -0,0 +1,21 @@
+import { httpClient } from './apiClient';
+
+export const getSchools = async (keyword: string): Promise<string[]> => {
+  if (!keyword.trim()) {
+    return [];
+  }
+
+  const data = await httpClient.searchSchool(keyword);
+  console.log(data);
+  return data;
+};
+
+export const getMajors = async (keyword: string): Promise<string[]> => {
+  if (!keyword.trim()) {
+    return [];
+  }
+
+  const data = await httpClient.searchMajor(keyword);
+  console.log(data);
+  return data;
+};
