2f9511979f15bf1b551ce4b957021164c8efd379
H0onnn <dukei2017@naver.com>
2023-10-04 15:37:14 +0900

    Feat: Input에 입력된 데이터만 받도록 함

--- src/components/auth/signup/SetupMajor.tsx
+++ src/components/auth/signup/SetupMajor.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useFocus from '../../../hooks/useFocus';
 import useSetupInput from '../../../hooks/useSetupInput';
+import useSchoolMajorData from '../../../hooks/useSchoolMajorData';
 import Modal from './Modal';
-import NoMajorModal from './NoMajorModal';
 import MainComment from '../MainComment';
 import Input from '../../public/Input';
 import Button from '../../public/Button';
-import InputMajor from './InputMajor';
 import { majorValidation } from '../../../utils/auth/validationRules';
 import { ProfileSetupStepInterface } from '../../../types/types';
 import { ButtonContainer, InputWrapper } from '../../../styles/styles';
@@ -20,63 +19,46 @@ const SetupMajor = ({ onNext }: ProfileSetupStepInterface) => {
     debouncedValue: debouncedMajorValue,
     inputchangeHandler,
     setValue,
+    value,
   } = useSetupInput('major', majorValidation);
 
   const { isFocus, onBlur, onFocus } = useFocus();
 
-  const [showNoMajorModal, setShowNoMajorModal] = useState<boolean>(false);
+  const { datas } = useSchoolMajorData('school', debouncedMajorValue);
 
-  const [isInputMode, setIsInputMode] = useState<boolean>(false);
+  const isValueInData = datas.includes(value);
 
-  // 검색된 값이 없을 때 NoMajorModal을 띄우기 위한 핸들러
-  const filteredDataHandler = (count: number) => {
-    if (!showNoMajorModal && count === 0) {
-      setShowNoMajorModal(true);
-    }
-  };
-
-  // NoMajorModal에서 학과를 직접 입력하기 위한 핸들러
-  const inputModeHandler = () => setIsInputMode(true);
+  const isNextButtonActive = isValueInData && majorStatus === 'success';
 
   return (
     <>
-      {isInputMode ? (
-        <InputMajor />
-      ) : (
-        <>
-          <MainComment style={{ fontSize: '20px' }} comment='학과를 선택해주세요' />
-          <InputWrapper>
-            <Input
-              type='major'
-              status={majorStatus}
-              {...register('major', majorValidation)}
-              errorMessage={
-                errors.major && typeof errors.major.message === 'string'
-                  ? errors.major.message
-                  : undefined
-              }
-              placeholder='학과 이름을 검색해주세요.'
-              onFocus={onFocus}
-              onChange={inputchangeHandler}
-            />
-            {isFocus && (
-              <Modal
-                fieldName='major'
-                searchValue={debouncedMajorValue}
-                keywordSelectHandler={keywordSelectHandler}
-                setValue={setValue}
-                onBlur={onBlur}
-                onFiltered={filteredDataHandler}
-              />
-            )}
-          </InputWrapper>
-          {showNoMajorModal && (
-            <NoMajorModal onInputMode={inputModeHandler} setModalState={setShowNoMajorModal} />
-          )}
-        </>
-      )}
+      <MainComment style={{ fontSize: '20px' }} comment='학과를 선택해주세요' />
+      <InputWrapper>
+        <Input
+          type='major'
+          status={majorStatus}
+          {...register('major', majorValidation)}
+          errorMessage={
+            errors.major && typeof errors.major.message === 'string'
+              ? errors.major.message
+              : undefined
+          }
+          placeholder='학과 이름을 검색해주세요.'
+          onFocus={onFocus}
+          onChange={inputchangeHandler}
+        />
+        {isFocus && (
+          <Modal
+            fieldName='major'
+            searchValue={debouncedMajorValue}
+            keywordSelectHandler={keywordSelectHandler}
+            setValue={setValue}
+            onBlur={onBlur}
+          />
+        )}
+      </InputWrapper>
       <ButtonContainer>
-        <Button $isFullWidth onClick={onNext} disabled={majorStatus !== 'success'}>
+        <Button $isFullWidth onClick={onNext} disabled={!isNextButtonActive}>
           다음
         </Button>
       </ButtonContainer>
