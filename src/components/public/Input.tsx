4f93fc5ab70f44ee8ac3ce34d55674266286b8c5
H0onnn <dukei2017@naver.com>
2023-10-06 14:45:45 +0900

    Feat: Input 컴포넌트 props 추가 및 반영

--- src/components/public/Input.tsx
+++ src/components/public/Input.tsx
@@ -8,10 +8,10 @@ import SearchIcon from '../../public/images/input/search.png';
 interface InputInterface extends React.InputHTMLAttributes<HTMLInputElement> {
   image?: string | React.ReactNode;
   button?: boolean;
-  children?: React.ReactNode;
+  buttonText?: string;
   status?: 'default' | 'success' | 'error' | 'search';
   errorMessage?: string;
-  isVerified?: boolean;
+  onButtonClick?: () => void;
 }
 
 const colorMap = {
@@ -22,7 +22,10 @@ const colorMap = {
 };
 
 const Input = forwardRef<HTMLInputElement, InputInterface>(
-  ({ image, button, children, status = 'default', errorMessage, isVerified, ...props }, ref) => {
+  (
+    { image, button, status = 'default', errorMessage, buttonText, onButtonClick, ...props },
+    ref,
+  ) => {
     const renderIcon = () => {
       if (button) return null;
 
@@ -46,10 +49,11 @@ const Input = forwardRef<HTMLInputElement, InputInterface>(
           {button && (
             <VerifiedButton
               variant='primary'
-              isSuccess={status === 'success'}
               disabled={status !== 'success'}
+              onClick={onButtonClick}
+              type='button'
             >
-              {isVerified ? '인증 완료' : children || '인증 메일 보내기'}
+              {buttonText}
             </VerifiedButton>
           )}
         </InputLayout>
@@ -102,7 +106,7 @@ const ErrorText = styled.p`
   bottom: -2rem;
 `;
 
-const VerifiedButton = styled(Button)<{ isSuccess: boolean }>`
+const VerifiedButton = styled(Button)`
   color: ${colors.inputFont};
   font-size: 12px;
   border-radius: 900px;
