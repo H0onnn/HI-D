eb2d19ce084fdc0c3c05b3cf7e9465c25c361da9
H0onnn <dukei2017@naver.com>
2023-10-06 14:39:31 +0900

    Refactor: constant url .env로 관리

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
-import DEFAULT_IMG from '../public/images/elephant.png';
+const { VITE_APP_DEFAULT_PROFILE_IMG, VITE_APP_API_BASE_URL } = import.meta.env;
 
 export const URL = {
-  DEFAULT_PROFILE_IMG: DEFAULT_IMG,
-  API_BASE_URL: 'http://43.201.168.231/api/',
+  DEFAULT_PROFILE_IMG: VITE_APP_DEFAULT_PROFILE_IMG,
+  API_BASE_URL: VITE_APP_API_BASE_URL,
 } as const;
