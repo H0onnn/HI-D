e244312cdaa0877a8bca554c8b09a9f408530ef5
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-10-09 01:47:39 +0900

    Chore: vite script 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
     port: 3000,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: 3000,
+    host: '0.0.0.0',
   },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
