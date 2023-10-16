69ce9df16e806f6dcc70e8645fce1f05d330ce2b
H0onnn <dukei2017@naver.com>
2023-10-16 19:38:24 +0900

    Feat: 전공 계열선택 컴포넌트 구현

---
+++ src/components/posting/SelectMajor.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { colors } from '@/constants/colors';
+import Button from '../public/Button';
+import { ButtonContainer } from '@/styles/styles';
+import SelectMajorList from './SelectMajorList';
+
+const SelectMajor = () => {
+  const [currentMajor, setCurrentMajor] = useState<string | null>(null);
+
+  return (
+    <>
+      <TitleContainer>
+        <MainTitle>질문과 관련된 전공을 선택해주세요</MainTitle>
+        <SubTitle>유용한 정보를 매칭해 드릴게요</SubTitle>
+      </TitleContainer>
+      <SelectMajorList onMajorSelect={setCurrentMajor} />
+      <ButtonContainer>
+        <Button $isFullWidth onClick={() => {}} disabled={!currentMajor}>
+          다음
+        </Button>
+      </ButtonContainer>
+    </>
+  );
+};
+
+export default SelectMajor;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 1.5rem;
+`;
+
+const MainTitle = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  color: ${colors.black};
+  line-height: 30px;
+`;
+
+const SubTitle = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${colors.gray5};
+  line-height: 24px;
+`;
