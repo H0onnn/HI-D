d06bc13dd76f19fd48798cd703b610812d7b28c3
H0onnn <dukei2017@naver.com>
2023-10-17 17:26:53 +0900

    Feat: 계열 아이템 컴포넌트 구현

---
+++ src/components/posting/EditMajorItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '@/constants/colors';
+import { useFormContext } from 'react-hook-form';
+
+interface EditMajorItemInterface {
+  major: string;
+  majorChangeHandler: (major: string) => void;
+}
+
+const EditMajorItem = ({ major, majorChangeHandler }: EditMajorItemInterface) => {
+  const { register } = useFormContext();
+
+  return (
+    <MajorItemWrapper onClick={() => majorChangeHandler(major)}>
+      <MajorItemInput {...register('major')} type='major_edit_hidden' value={major} />
+      {major}
+    </MajorItemWrapper>
+  );
+};
+
+export default EditMajorItem;
+
+const MajorItemWrapper = styled.div`
+  width: 20rem;
+  height: 4.8rem;
+  border: 0.5px solid ${colors.gray2};
+  font-size: 14px;
+  color: ${colors.gray5};
+  line-height: 21px;
+  display: flex;
+  align-items: center;
+  padding: 1rem 1.6rem;
+
+  &:hover {
+    border-color: ${colors.third};
+    background-color: ${colors.pastel};
+    color: ${colors.primary};
+  }
+`;
+
+const MajorItemInput = styled.input`
+  display: none;
+`;
