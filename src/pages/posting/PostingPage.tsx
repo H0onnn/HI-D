0183d4b35a67436d913a3ad44bfa919510397d4e
H0onnn <dukei2017@naver.com>
2023-10-18 21:13:33 +0900

    Feat: 게시글 작성 페이지 컴포넌트 구현

--- src/pages/posting/PostingPage.tsx
+++ src/pages/posting/PostingPage.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useFunnel } from '@/hooks/useFunnel';
 import { PageLayout } from '@/styles/styles';
 import PageHeader from '@/components/public/PageHeader';
 import PostingForm from '@/components/posting/PostingForm';
 import PostingSetup from '@/components/posting/PostingSetup';
+import {
+  postingNextClickHandler,
+  postingPrevClickHandler,
+  postingSubmit,
+} from '@/services/postingService';
+import { LINK } from '@/constants/links';
 
 const PostingPage = () => {
-  const steps = ['계열 선택', '게시글 작성'];
+  const navigate = useNavigate();
 
-  const { Funnel, Step } = useFunnel(steps[0]);
+  const location = useLocation();
+
+  const currentPath = location.pathname;
+
+  const isHelpPost = currentPath === LINK.POSTING_HELP;
+  const isFreePost = currentPath === LINK.POSTING_FREE;
+
+  const steps = ['계열 선택', '게시글 작성'];
+  const defaultStep = isHelpPost ? steps[0] : steps[1];
+
+  const { Funnel, Step, setStep, currentStep } = useFunnel(defaultStep);
+
+  const nextClickHandler = postingNextClickHandler(setStep, steps);
+
+  const prevClickHandler = postingPrevClickHandler(setStep, steps, navigate, currentPath);
 
   return (
     <>
-      <PageHeader title='게시글 작성' onClick={() => {}} />
+      <PageHeader title='게시글 작성' onClick={() => prevClickHandler(currentStep)} />
       <PageLayout>
-        <PostingForm onSubmit={() => {}}>
-          <PostingSetup steps={steps} Funnel={Funnel} Step={Step} />
+        <PostingForm onSubmit={postingSubmit}>
+          <PostingSetup
+            steps={steps}
+            Funnel={Funnel}
+            Step={Step}
+            nextClickHandler={nextClickHandler}
+            isFreePost={isFreePost}
+            isHelpPost={isHelpPost}
+          />
         </PostingForm>
       </PageLayout>
     </>
