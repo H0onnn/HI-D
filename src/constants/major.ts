d03e2196af824f2309d15f1d2e8ac7ac1e992419
H0onnn <dukei2017@naver.com>
2023-10-20 14:54:06 +0900

    Refactor: 계열에 따른 데이터 값을 영어로 보내주기 위한 매핑객체 추가

--- src/constants/major.ts
+++ src/constants/major.ts
@@ -6,3 +6,14 @@ export const MAJORS = [
   '간호, 의약, 의학 계열',
   '기타',
 ] as const;
+
+export type MajorKeys = (typeof MAJORS)[number];
+
+export const majorToEnglishMapping: Record<MajorKeys, string | undefined> = {
+  '인문, 경영, 사회 계열': 'SOCIAL',
+  '자연, 컴퓨터, 공학 계열': 'ENGINEERING',
+  '교대, 사범대, 교육 계열': 'EDUCATION',
+  '음악, 미술, 체육 예체능 계열': 'PSYCHICAL',
+  '간호, 의약, 의학 계열': 'MEDICAL',
+  기타: undefined,
+} as const;
