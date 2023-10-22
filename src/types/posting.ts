400a7433b913a2b5dd282e32e9a2ec969942ba1d
H0onnn <dukei2017@naver.com>
2023-10-22 22:45:16 +0900

    Refactor: 게시글 데이터 형식 수정

--- src/types/posting.ts
+++ src/types/posting.ts
@@ -2,7 +2,6 @@ export interface PostingDataInterface {
   major?: string;
   title: string;
   content: string;
-  boardType: string;
   imageUrls?: string[];
   isAnonymous: boolean;
   tag?: string;
