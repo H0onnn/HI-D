8d07178d6c8dbb05c14311fdb736124662b394e3
H0onnn <dukei2017@naver.com>
2023-10-22 22:25:28 +0900

    Refactor: LINK constant 적용

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import useAuthStore from '@/store/authStore';
+import { LINK } from '@/constants/links';
 
 const ProtectedRoute = () => {
   const isAuth = useAuthStore((state) => state.token);
 
-  return isAuth ? <Outlet /> : <Navigate to='/login' replace />;
+  return isAuth ? <Outlet /> : <Navigate to={LINK.LOGIN} replace />;
 };
 
 export default ProtectedRoute;
