812668a5ad69b338ac686fc1395e8a22bf428414
H0onnn <dukei2017@naver.com>
2023-10-24 16:34:59 +0900

    Feat: comment item listing 컴포넌트 구현

---
+++ src/components/comment/listing/CommentList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { colors } from '@/constants/colors';
+import CommentItem from './CommentItem';
+import CommentTextArea from '../contentArea/CommentTextArea';
+import { CommentDataInterface } from '@/types/comment';
+
+interface CommentListInterface {
+  commentList: CommentDataInterface[];
+  postId: number;
+}
+
+const CommentList = ({ commentList, postId }: CommentListInterface) => {
+  const [comments, setComments] = useState<CommentDataInterface[]>(commentList);
+
+  return (
+    <>
+      <CommentListLayout $commentsEmpty={comments.length === 0}>
+        {comments.map((comment) => (
+          <CommentItem
+            key={comment.replyId}
+            writer_image={comment.writer.imageUrl}
+            writer_name={comment.writer.nickname}
+            content={comment.content}
+            created_at={comment.createdAt}
+            comment_like={comment.recommendCount}
+          />
+        ))}
+      </CommentListLayout>
+      <CommentTextArea
+        postId={postId}
+        onNewComment={(newComment) => setComments((prevComments) => [...prevComments, newComment])}
+      />
+    </>
+  );
+};
+
+export default CommentList;
+
+const CommentListLayout = styled.div<{ $commentsEmpty: boolean }>`
+  width: 100%;
+  border-radius: 8px;
+  padding: 1.5rem;
+  background-color: ${(props) => (props.$commentsEmpty ? 'transparent' : colors.gray1)};
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
