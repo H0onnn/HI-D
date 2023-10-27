b9ddf358f7dcd65b1ae6a9812e08b5efa3648352
H0onnn <dukei2017@naver.com>
2023-10-27 14:33:39 +0900

    Feat: react query provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './layout/Layout';
 import AppRoutes from './router/routes';
 import ToasterContext from './libs/ToasterContext';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <Router>
-      <Layout>
-        <ToasterContext />
-        <AppRoutes />
-      </Layout>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Layout>
+          <ToasterContext />
+          <AppRoutes />
+        </Layout>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
