5fa0d8b32c96e6c188c7c683aa325ec7d5a8e55e
H0onnn <dukei2017@naver.com>
2023-10-29 21:14:20 +0900

    Remove: 미사용 state 삭제

--- src/hooks/usePostActionState.ts
+++ src/hooks/usePostActionState.ts
@@ -5,25 +5,21 @@ const usePostActionState = () => {
   const [isBookMarked, setIsBookMarked] = useState<boolean>(false);
   const [isReported, setIsReported] = useState<boolean>(false);
   const [isMoreActions, setIsMoreActions] = useState<boolean>(false);
-  const [isEditing, setIsEditing] = useState<boolean>(false);
 
   const toggleLike = () => setIsLiked((prev) => !prev);
   const toggleBookmark = () => setIsBookMarked((prev) => !prev);
   const toggleReport = () => setIsReported((prev) => !prev);
   const toggleMoreActions = () => setIsMoreActions((prev) => !prev);
-  const toggleEditPost = () => setIsEditing((prev) => !prev);
 
   return {
     isLiked,
     isBookMarked,
     isReported,
     isMoreActions,
-    isEditing,
     toggleLike,
     toggleBookmark,
     toggleReport,
     toggleMoreActions,
-    toggleEditPost,
   };
 };
 
