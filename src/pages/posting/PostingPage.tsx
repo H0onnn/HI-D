ed37a744a14116edf0a61255fb5d62c4442f1d2a
H0onnn <dukei2017@naver.com>
2023-10-30 17:41:36 +0900

    Feat: reqct query mutate를 이용한 게시글 수정 로직 적용

--- src/pages/posting/PostingPage.tsx
+++ src/pages/posting/PostingPage.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useFunnel } from '@/hooks/useFunnel';
+import useSubmitPost from '@/hooks/useSubmitPost';
 import { PageLayout } from '@/styles/styles';
 import PageHeader from '@/components/public/PageHeader';
 import PostingForm from '@/components/posting/PostingForm';
 import PostingSetup from '@/components/posting/PostingSetup';
-import { handleNextClick, handlePrevClick, submitPosting } from '@/services/postingService';
+import { handleNextClick, handlePrevClick } from '@/services/posting';
 import { LINK } from '@/constants/links';
+import { PostingDataInterface } from '@/types/posting';
 
 const PostingPage = () => {
   const navigate = useNavigate();
-
   const location = useLocation();
+  const { submitPost, editPostMutation } = useSubmitPost();
 
   const currentPath = location.pathname;
   const postToEdit = location.state?.post;
 
-  const isHelpPost = currentPath === LINK.POSTING_HELP || postToEdit?.boardType === 'needhelp';
-  const isFreePost = currentPath === LINK.POSTING_FREE || postToEdit?.boardType === 'free';
+  const isHelpPost = currentPath === LINK.POSTING_HELP || postToEdit?.boardType === 'NEED_HELP';
+  const isFreePost = currentPath === LINK.POSTING_FREE || postToEdit?.boardType === 'FREE';
   const submitType = isHelpPost ? 'needhelp' : 'free';
 
   const steps = ['계열 선택', '게시글 작성'];
@@ -26,14 +28,21 @@ const PostingPage = () => {
   const { Funnel, Step, setStep, currentStep } = useFunnel(defaultStep);
 
   const nextClickHandler = handleNextClick(setStep, steps);
-
   const prevClickHandler = handlePrevClick(setStep, steps, navigate, currentPath);
 
   return (
     <>
       <PageHeader title='게시글 작성' onClick={() => prevClickHandler(currentStep)} />
       <PageLayout>
-        <PostingForm onSubmit={submitPosting(submitType, navigate)}>
+        <PostingForm
+          postToEdit={postToEdit}
+          onSubmit={
+            postToEdit
+              ? (data: PostingDataInterface) =>
+                  editPostMutation.mutate({ postId: postToEdit.postId, data })
+              : submitPost(submitType)
+          }
+        >
           <PostingSetup
             steps={steps}
             Funnel={Funnel}
