5b7aeaca63802e896dfef0042290e4663e296da3
H0onnn <dukei2017@naver.com>
2023-10-31 15:23:28 +0900

    Feat: 게시글 신고 handler 함수 작성

--- src/hooks/usePostActionHandlers.ts
+++ src/hooks/usePostActionHandlers.ts
@@ -1,9 +1,11 @@
+// import {useQueryClient, useMutation} from '@tanstack/react-query';
 import usePostActionState from './usePostActionState';
 import { useNavigate } from 'react-router-dom';
-import { postLike, postBookmark, postDelete } from '@/services/postActions';
+import { postLike, postBookmark, postReport, postDelete } from '@/services/postActions';
 import { LINK } from '@/constants/links';
 import toast from 'react-hot-toast';
 import { PostingDataInterface } from '@/types/posting';
+import { ReportDataInterface } from '@/types/report';
 
 const usePostActionHandlers = () => {
   const { toggleBookmark, toggleLike } = usePostActionState();
@@ -19,6 +21,15 @@ const usePostActionHandlers = () => {
     await postBookmark(postId);
   };
 
+  const reportPost = async (postId: number, data: ReportDataInterface) => {
+    try {
+      await postReport(postId, data);
+      toast.success('신고가 접수되었어요.', { id: 'postReportSuccess' });
+    } catch (err: unknown) {
+      toast.error('신고 접수 중 오류가 발생했습니다.', { id: 'postReportFail' });
+    }
+  };
+
   const editPost = (post: PostingDataInterface, postId: number) => {
     navigate(LINK.POST_EDIT.replace(':id', postId.toString()), {
       state: { post },
@@ -28,7 +39,7 @@ const usePostActionHandlers = () => {
 
   const deletePost = async (postId: number) => {
     try {
-      if (window.confirm('해당 게시글을 삭제하시겠습니까?')) {
+      if (window.confirm('해당 게시글을 삭제하시겠어요?')) {
         await postDelete(postId);
         navigate(LINK.MAIN);
         toast.success('게시글이 삭제되었습니다.', { id: 'postDeleteSuccess' });
@@ -42,6 +53,7 @@ const usePostActionHandlers = () => {
   return {
     likePost,
     bookmarkPost,
+    reportPost,
     editPost,
     deletePost,
   };
