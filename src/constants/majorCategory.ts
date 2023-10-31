66c61617a950cfa6cf99bf1fcad44ff46d2504c9
H0onnn <dukei2017@naver.com>
2023-10-31 15:21:17 +0900

    Refactor: 전공 계열 기타 선택시 undefined -> ETC 수정

--- src/constants/majorCategory.ts
+++ src/constants/majorCategory.ts
@@ -9,13 +9,13 @@ export const MAJORS = [
 
 export type MajorKeys = (typeof MAJORS)[number];
 
-export const majorToEnglishMapping: Record<MajorKeys, string | undefined> = {
+export const majorToEnglishMapping: Record<MajorKeys, string> = {
   '인문, 경영, 사회 계열': 'SOCIAL',
   '자연, 컴퓨터, 공학 계열': 'ENGINEERING',
   '교대, 사범대, 교육 계열': 'EDUCATION',
   '음악, 미술, 체육 예체능 계열': 'PHYSICAL',
   '간호, 의약, 의학 계열': 'MEDICAL',
-  기타: undefined,
+  기타: 'ETC',
 } as const;
 
 export const majorToKoreaMapping: Record<string, MajorKeys> = {
@@ -24,5 +24,5 @@ export const majorToKoreaMapping: Record<string, MajorKeys> = {
   EDUCATION: '교대, 사범대, 교육 계열',
   PHYSICAL: '음악, 미술, 체육 예체능 계열',
   MEDICAL: '간호, 의약, 의학 계열',
-  undefined: '기타',
+  ETC: '기타',
 } as const;
