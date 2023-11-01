a4e38aa8dd67afb93661a019cea97aa63587d29b
H0onnn <dukei2017@naver.com>
2023-11-01 16:35:52 +0900

    Feat: 게시글 좋아요, 북마크 기능 완성

--- src/hooks/usePostActionHandlers.ts
+++ src/hooks/usePostActionHandlers.ts
@@ -1,4 +1,4 @@
-// import {useQueryClient, useMutation} from '@tanstack/react-query';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
 import usePostActionState from './usePostActionState';
 import { useNavigate } from 'react-router-dom';
 import { postLike, postBookmark, postReport, postDelete } from '@/services/postActions';
@@ -6,24 +6,68 @@ import { LINK } from '@/constants/links';
 import toast from 'react-hot-toast';
 import { PostingDataInterface } from '@/types/posting';
 import { ReportDataInterface } from '@/types/report';
+import { PostDetailInterface } from '@/types/post';
 
 const usePostActionHandlers = () => {
-  const { toggleBookmark, toggleLike } = usePostActionState();
+  const { toggleReport } = usePostActionState();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
-  const likePost = async (postId: number) => {
-    toggleLike();
-    await postLike(postId);
+  const likePostMutation = useMutation<void, unknown, number, { postId: number }>({
+    mutationFn: async (postId) => {
+      await postLike(postId);
+    },
+    onSuccess: (_, postId) => {
+      queryClient.invalidateQueries({ queryKey: ['postDetailData', postId] });
+    },
+  });
+
+  const likePost = (postId: number) => {
+    const currentData = queryClient.getQueryData(['postDetailData', postId]) as
+      | PostDetailInterface
+      | undefined;
+    if (currentData) {
+      const updatedData = {
+        ...currentData,
+        likesCount: currentData.isRecommended
+          ? currentData.recommendCount - 1
+          : currentData.recommendCount + 1,
+        isLiked: !currentData.isRecommended,
+      };
+      queryClient.setQueryData(['postDetailData', postId], updatedData);
+    }
+
+    likePostMutation.mutate(postId);
   };
 
-  const bookmarkPost = async (postId: number) => {
-    toggleBookmark();
-    await postBookmark(postId);
+  const bookmarkPostMutation = useMutation<void, unknown, number, { postId: number }>({
+    mutationFn: async (postId) => {
+      await postBookmark(postId);
+    },
+    onSuccess: (_, postId) => {
+      queryClient.invalidateQueries({ queryKey: ['postDetailData', postId] });
+    },
+  });
+
+  const bookmarkPost = (postId: number) => {
+    const currentData = queryClient.getQueryData(['postDetailData', postId]) as
+      | PostDetailInterface
+      | undefined;
+    if (currentData) {
+      const updatedData = {
+        ...currentData,
+        isBookmarked: !currentData.isBookmarked,
+      };
+      queryClient.setQueryData(['postDetailData', postId], updatedData);
+    }
+
+    bookmarkPostMutation.mutate(postId);
   };
 
   const reportPost = async (postId: number, data: ReportDataInterface) => {
     try {
       await postReport(postId, data);
+      toggleReport();
       toast.success('신고가 접수되었어요.', { id: 'postReportSuccess' });
     } catch (err: unknown) {
       toast.error('신고 접수 중 오류가 발생했습니다.', { id: 'postReportFail' });
