64d44b234ed5353fb249d685f4abfda5f9e262df
H0onnn <dukei2017@naver.com>
2023-11-06 19:49:37 +0900

    Refactor: 랜덤닉네임이 최소 2 - 최대 8글자로 생성되도록 수정

--- src/utils/randomNick.ts
+++ src/utils/randomNick.ts
@@ -14,9 +14,9 @@ export const generateRandomNickname = (): string => {
     'Yale',
     'Stanford',
     'MIT',
-    'Princeton',
+    'Princet',
     'Oxford',
-    'Cambridge',
+    'Camb',
     'Caltech',
     'UCLA',
     'Berkeley',
@@ -27,8 +27,17 @@ export const generateRandomNickname = (): string => {
   // 랜덤한 이름 부분 선택
   const namePart = getRandomItem(names);
 
-  // 3자리 랜덤 숫자 부분 생성
-  const numberPart = Math.floor(100 + Math.random() * 900);
+  // 남은 길이 계산 (최소 2글자에서 최대 8글자 사이여야 함)
+  const remainingLength = 8 - namePart.length;
+
+  // 남은 길이에 따라 랜덤 숫자 부분 생성
+  let numberPart = '';
+  if (remainingLength > 0) {
+    // 남은 길이만큼 랜덤 숫자 생성
+    const min = Math.pow(10, remainingLength - 1); // 남은 길이에 맞는 최소 숫자
+    const max = Math.pow(10, remainingLength) - 1; // 남은 길이에 맞는 최대 숫자
+    numberPart = Math.floor(min + Math.random() * (max - min + 1)).toString();
+  }
 
   return `${namePart}${numberPart}`;
 };
