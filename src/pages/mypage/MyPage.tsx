55b2a02c3b449b85d1fe45522e1bd8da227327ee
H0onnn <dukei2017@naver.com>
2023-11-08 23:36:09 +0900

    Refactor: 로그아웃 로직 훅으로 분리

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,48 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useLogout } from '@/store/authStore';
-import useModalStore from '@/store/modalStore';
-import { postLogout } from '@/services/user';
+import useMyPageActions from '@/hooks/useDeleteAccount';
 import styled from 'styled-components';
 import { ButtonContainer } from '@/styles/styles';
 import Button from '@/components/public/Button';
 import PageHeader from '@/components/public/PageHeader';
 import Background from '@/components/mypage/background/Background';
 import Contents from '@/components/mypage/contents/Contents';
-import DeleteAccountBtn from '@/components/mypage/actions/DeleteAccountBtn';
-import { MODAL_TYPES } from '@/types/modal';
-import { LINK } from '@/constants/links';
-import toast from 'react-hot-toast';
+import DeleteAccountBtn from '@/components/mypage/actions/deleteAccount/DeleteAccountBtn';
 
 const MyPage = () => {
-  const logout = useLogout();
-  const { openModal, closeModal } = useModalStore();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    try {
-      await postLogout();
-      logout();
-      closeModal();
-      navigate(LINK.LOGIN);
-      toast.success('로그아웃 되었습니다.', { id: 'logoutSuccess' });
-    } catch (err: unknown) {
-      console.error('로그아웃 에러 : ', err);
-      toast.error('로그아웃에 실패했어요.', { id: 'logoutFail' });
-    }
-  };
-
-  const logoutModalHandler = () => {
-    openModal({
-      modalType: MODAL_TYPES.ALERT,
-      modalProps: {
-        title: '정말 로그아웃 하시겠어요?',
-        content: '로그아웃시 로그인 페이지로 이동합니다.',
-        confirmText: '로그아웃',
-        onConfirmHandler: handleLogout,
-      },
-    });
-  };
+  const { logoutModalHandler } = useMyPageActions();
 
   return (
     <>
