89e3e5a2a6257e53aed0178220a85a05aae474f4
H0onnn <dukei2017@naver.com>
2023-11-08 23:29:08 +0900

    Refactor: setup 로직 별도 파일로 분리 및 공용화

--- src/services/posting.ts
+++ src/services/posting.ts
@@ -1,7 +1,5 @@
-import { NavigateFunction } from 'react-router-dom';
 import { PostingDataInterface } from '@/types/posting';
 import { httpClient } from '../api/httpClient';
-import { LINK } from '@/constants/links';
 import { PostDetailInterface } from '@/types/post';
 
 export const postNeedHelp = async (data: PostingDataInterface): Promise<PostDetailInterface> => {
@@ -34,58 +32,3 @@ export const patchPost = async (postId: number, data: PostingDataInterface): Pro
     throw err;
   }
 };
-
-const getCurrentStepIndex = (currentStep: string, steps: string[]) => steps.indexOf(currentStep);
-
-const getNextStepIndex = (currentStep: string, steps: string[]) =>
-  getCurrentStepIndex(currentStep, steps) + 1;
-
-const getPrevStepIndex = (currentStep: string, steps: string[]) =>
-  getCurrentStepIndex(currentStep, steps) - 1;
-
-const shouldNavigateAway = (
-  currentStep: string,
-  steps: string[],
-  confirmMessage: string,
-  navigate: NavigateFunction,
-  currentPath?: string,
-) => {
-  const isHelpPost = currentPath === LINK.POSTING_HELP;
-  const availableSteps = isHelpPost ? steps : steps.slice(1);
-  const currentStepIndex = availableSteps.indexOf(currentStep);
-  const isFirstStep = currentStepIndex === 0;
-
-  if (isFirstStep && window.confirm(confirmMessage)) {
-    navigate(LINK.MAIN);
-    return true;
-  }
-
-  return false;
-};
-
-export const handleNextClick =
-  (setStep: (step: string) => void, steps: string[]) => (currentStep: string) => {
-    const nextStepIndex = getNextStepIndex(currentStep, steps);
-    if (nextStepIndex < steps.length) {
-      setStep(steps[nextStepIndex]);
-    }
-  };
-
-export const handlePrevClick =
-  (
-    setStep: (step: string) => void,
-    steps: string[],
-    confirmMessage: string,
-    navigate: NavigateFunction,
-    currentPath?: string,
-  ) =>
-  (currentStep: string) => {
-    if (shouldNavigateAway(currentStep, steps, confirmMessage, navigate, currentPath)) {
-      return;
-    }
-
-    const prevStepIndex = getPrevStepIndex(currentStep, steps);
-    if (prevStepIndex >= 0) {
-      setStep(steps[prevStepIndex]);
-    }
-  };
