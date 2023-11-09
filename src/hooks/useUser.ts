67bd876b5e11c0ebb4468d531e8cbcf9571fedf4
H0onnn <dukei2017@naver.com>
2023-11-09 13:30:41 +0900

    Feat: 마이페이지 접속시 user query invalidate

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,14 +1,26 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useAuthToken } from '@/store/authStore';
 import { useQuery, useQueryClient, QueryClient } from '@tanstack/react-query';
 import { getUserData } from '@/services/user';
 import { UserDataInterface } from '@/types/user';
+import { LINK } from '@/constants/links';
+
+export const QUERY_KEY = 'currentUser';
 
 const useUser = () => {
   const queryClient: QueryClient = useQueryClient();
   const token = useAuthToken();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === LINK.MYPAGE) {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY, token] });
+    }
+  }, [location.pathname, queryClient, token]);
 
   const { data: user } = useQuery<UserDataInterface>({
-    queryKey: ['currnetUser'],
+    queryKey: [QUERY_KEY, token],
     queryFn: getUserData,
     enabled: !!token,
     staleTime: 1000 * 60 * 60, // 1시간
