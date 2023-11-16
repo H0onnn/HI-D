867494308aa4ffce0aa153d4215e954f10ab7030
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-11-17 01:36:38 +0900

    Feat: chat 상대방 입장시 최근메시지 api 요청

--- src/store/chatMessageStore.ts
+++ src/store/chatMessageStore.ts
@@ -3,18 +3,28 @@ import { create } from 'zustand';
 
 interface MessageStore {
   messages: MessageInterface[];
+  enterMember: string;
   pushMessage: (message: MessageInterface) => void;
   initMessages: () => void;
 }
 
 export const useChatMessageStore = create<MessageStore>((set) => ({
   messages: [],
-  pushMessage: (message) =>
-    set((state) => ({
-      messages: [...state.messages, message],
-    })),
+  enterMember: '',
+  pushMessage: (message) => {
+    if (message.type === 'ENTER') {
+      set(() => ({
+        messages: [],
+        enterMember: message.sender,
+      }));
+    } else
+      set((state) => ({
+        messages: [...state.messages, message],
+      }));
+  },
   initMessages: () =>
     set(() => ({
       messages: [],
+      enterMember: '',
     })),
 }));
