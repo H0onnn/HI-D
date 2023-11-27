4367d83563529b0ee8a47046cbafa7a58aef9c04
H0onnn <dukei2017@naver.com>
2023-11-27 19:01:05 +0900

    Feat: 알림 삭제 서비스 로직 추가

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -5,3 +5,21 @@ export const getNotifications = async (): Promise<NotificationInterface> => {
   const response = await httpClient.notification.get.notification();
   return response.data;
 };
+
+export const deleteNotificationById = async (notificationId: number): Promise<void> => {
+  try {
+    await httpClient.notification.delete.notification(notificationId);
+  } catch (err: unknown) {
+    console.error('알림 삭제 에러 : ', err);
+    throw err;
+  }
+};
+
+export const deleteAllNotificationsByIds = async (notificationIds: number[]): Promise<void> => {
+  try {
+    await httpClient.notification.delete.allNotifications(notificationIds);
+  } catch (err: unknown) {
+    console.error('전체 알림 삭제 에러 : ', err);
+    throw err;
+  }
+};
